d1b2db46636db2c490fc1ca5133f30790c423ff0
Tyler Mazur <tylerm02192009@gmail.com>
2022-11-17 23:33:47 -0800

    9

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const contentSplit = content.split(" ")
   else if (content === "philip just keep pick pick picking those cherries" || content === "philip just keep pick pick pick picking those cherries") {
     message.reply(":sob: SHUTTTTTTTTTTTTT UPPPPPPPPPPPPPPPPPP") 
     message.channel.send("https://tenor.com/view/leaves-leaves-room-leaves-chat-left-escape-gif-24873136")
-  } else if (message.content === "<@1042237331257688104> resonses") {
+  } else if (message.content === ";responses") {
     message.reply('__**Responses**__/n*no capitals required*/n/nTrigger: "I hate Philip" /nResponse: "FUCK YOU"/n/nTrigger: "Say the N word Philip" OR "Philip say the N word"/nResponse: ":middle_finger:"/n/nTrigger: "Philip you love your Minecraft gf Gabi" OR "Philips minecraft gf is Gabi"/nResponse: Headlock GIF OR Agressive Headlock GIF OR I "WILL SLAP YOU WITH A RULER!!!!!"/n/nTrigger: "Philip just keep pick pick picking those cherries" OR "Philip just keep pick pick pick picking those cherries"/nResponse: ":sob: SHUTTTTTTTTTTTTT UPPPPPPPPPPPPPPPPPP" AND Leaving GIF/n/n**Feel free to recommend more in <#998774406828527677**')
   }
   }
