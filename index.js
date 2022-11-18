df18639b805c8fbdc95b30dacef4c531a49dc0c3
Tyler Mazur <tylerm02192009@gmail.com>
2022-11-17 21:57:09 -0800

    3

--- index.js
+++ index.js
@@ -40,15 +40,3 @@ const contentSplit = content.split(" ")
          }
   })
 client.login(process.env.TOKEN)
-
-
-const express = require('express')
-const app = express();
-const port = 3000;
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`)
-})
-
